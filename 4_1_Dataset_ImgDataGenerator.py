a44a6716f3ca0bb2d53faa1429661dd99289612f
H-Li24 <hench_li@163.com>
2021-04-28 21:58:52 +0900

    master

---
+++ 4_1_Dataset_ImgDataGenerator.py
@@ -0,0 +1,17 @@
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+img_gen = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1./255, rotation_range=20)
+
+flowers = './flower_photos/flower_photos'
+
+def Gen():
+    gen = img_gen.flow_from_directory(flowers)
+    for (x,y) in gen:
+        yield (x,y)
+
+flower_dataset = tf.data.Dataset.from_generator(Gen, output_types=(tf.float32, tf.float32))
+
+for image, label in flower_dataset:
+    print(image.shape, label.shape)
+    break
---
+++ 4_1_Dataset_from_tensor.py
@@ -0,0 +1,36 @@
+import tensorflow as tf 
+import numpy as np
+
+# one data
+
+dataset1 = tf.data.Dataset.from_tensors(np.zeros(shape=(10,5,2), dtype=np.float32))
+flag = 0
+for element in dataset1:
+    flag += 1
+    print(element.shape)
+    print(flag)
+
+dataset2 = tf.data.Dataset.from_tensor_slices(np.zeros(shape=(10,5,2), dtype=np.float32))
+flag = 0
+for element in dataset2:
+    flag += 1
+    print(element.shape)
+    print(flag)
+'''
+# two data
+
+dataset3 = tf.data.Dataset.from_tensors({"a":np.zeros(shape=(10,5,2), dtype=np.float32), "b":np.zeros(shape=(10,5,2), dtype=np.float32)}) # 传入是字典，传出是规整好的字典
+flag = 0
+for element in dataset3:
+    flag += 1
+    print(element["a"].shape, element["b"].shape)
+    print(flag)
+
+dataset3 = tf.data.Dataset.from_tensor_slices({"a":np.zeros(shape=(10,5,2), dtype=np.float32), "b":np.zeros(shape=(10,5,2), dtype=np.float32)}) # 传入是字典，传出是规整好的字典
+flag = 0
+for element in dataset3:
+    flag += 1
+    print(element["a"].shape, element["b"].shape)
+    print(flag)
+
+'''
